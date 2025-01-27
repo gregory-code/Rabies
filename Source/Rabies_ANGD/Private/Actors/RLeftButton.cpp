0aa617f292ee365b0d17eded0a41913e2fb1aa71
gregory-code <111695375+gregory-code@users.noreply.github.com>
2025-01-26 20:47:31 -0600

    Finished character select massive framework

--- Source/Rabies_ANGD/Private/Actors/RLeftButton.cpp
+++ Source/Rabies_ANGD/Private/Actors/RLeftButton.cpp
@@ -3,4 +3,18 @@
 
 #include "Actors/RLeftButton.h"
 #include "Player/RCharacterSelectController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/Engine.h"
 
+void ARLeftButton::OnActorClicked(AActor* TouchedActor, FKey ButtonPressed)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("LeftButton clicked!"));
+	}
+}
+
+void ARLeftButton::NotifyActorOnClicked(FKey ButtonPressed)
+{
+	OnActorClicked(this, ButtonPressed);
+}
